a296fc296e4d5c81bfe01fec8d4bd310d61db22b
jerffrey <jerffrey_167@163.com>
2022-09-25 20:45:49 +0800

    上传class定位

---
+++ find-class.py
@@ -0,0 +1,20 @@
+from  selenium import  webdriver
+from  time import  sleep
+from  selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+# 打开浏览器
+driver.maximize_window()
+#浏览器窗口最大化
+driver.get('http://www.baidu.com')
+#浏览器打开百度
+sleep(2)
+driver.find_element(By.CLASS_NAME,'s_ipt').send_keys('class定位')
+#class定位搜索框输入 class定位
+sleep(2)
+driver.find_element(By.CLASS_NAME,'s_btn').click()
+#class定位搜索按钮  注意当class的那么有多个数值时，只要输入其中任何一个就行，但是输入全部是不行的
+driver.close()
+#关闭浏览器器进程
+driver.quit()
+#关闭浏览器
