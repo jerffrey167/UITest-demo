d4d22219ff6812f8069fccc17aaa4077404f933a
jerffrey <jerffrey_167@163.com>
2022-09-26 10:43:35 +0800

    上传xpath定位 90% 进度

--- find-xpath.py
+++ find-xpath.py
@@ -5,7 +5,7 @@ driver = webdriver.Chrome()
 # 打开浏览器
 driver.maximize_window()
 #浏览器窗口最大化
-driver.get('http://120.46.215.163:8101/#/login')
+driver.get('http://192.168.3.12:8100/#/login')
 #浏览器打开百度
 sleep(2)
 driver.find_element(By.XPATH,'//input[@type="text"]').send_keys('person')
@@ -27,10 +27,77 @@ sleep(1)
 driver.find_element(By.XPATH,"//*[text()=' 开始考试 ']").click()
 #点击开始考试按钮
 sleep(3)
-
-driver.find_element(By.XPATH,"//*[text()='A.25%  ']//..").click()
-#第一题
-
+########
+#单选题
+#######
+driver.find_element(By.XPATH,"//*[contains(text(),'A.')]//..").click()
+sleep(1)
+driver.find_element(By.XPATH,"//span[text()=' 下一题 ']//..").click()
+#第一题 选A
+sleep(3)
+driver.find_element(By.XPATH,"//*[contains(text(),'B.')]//..").click()
+sleep(1)
+driver.find_element(By.XPATH,"//span[text()=' 下一题 ']//..").click()
+#第二题 选B
+sleep(3)
+driver.find_element(By.XPATH,"//*[contains(text(),'C.')]//..").click()
+sleep(1)
+driver.find_element(By.XPATH,"//span[text()=' 下一题 ']//..").click()
+#第三题 选C
+sleep(3)
+driver.find_element(By.XPATH,"//*[contains(text(),'D.')]//..").click()
+sleep(1)
+driver.find_element(By.XPATH,"//span[text()=' 下一题 ']//..").click()
+#第四题 选D
+sleep(3)
+driver.find_element(By.XPATH,"//*[contains(text(),'A.')]//..").click()
+sleep(1)
+driver.find_element(By.XPATH,"//span[text()=' 下一题 ']//..").click()
+#第五题 选A
+sleep(3)
+driver.find_element(By.XPATH,"//*[contains(text(),'B.')]//..").click()
+sleep(1)
+driver.find_element(By.XPATH,"//span[text()=' 下一题 ']//..").click()
+#第六题 选B
+#######
+#多选题
+#######
+sleep(3)
+driver.find_element(By.XPATH,"//*[contains(text(),'A.')]//..").click()
+driver.find_element(By.XPATH,"//*[contains(text(),'B.')]//..").click()
+driver.find_element(By.XPATH,"//*[contains(text(),'C.')]//..").click()
+sleep(1)
+driver.find_element(By.XPATH,"//span[text()=' 下一题 ']//..").click()
+#第七题 选A B C
+sleep(1)
+driver.find_element(By.XPATH,"//*[contains(text(),'B.')]//..").click()
+driver.find_element(By.XPATH,"//*[contains(text(),'C.')]//..").click()
+driver.find_element(By.XPATH,"//*[contains(text(),'D.')]//..").click()
+sleep(1)
+driver.find_element(By.XPATH,"//span[text()=' 下一题 ']//..").click()
+#第八题 选B C D
+#######
+#判断题
+#######
+sleep(1)
+driver.find_element(By.XPATH,"//*[contains(text(),'A.')]//..").click()
+sleep(1)
+driver.find_element(By.XPATH,"//span[text()=' 下一题 ']//..").click()
+#第九题 选A
+sleep(1)
+driver.find_element(By.XPATH,"//*[contains(text(),'B.')]//..").click()
+#第十题 选B
+##################################交卷
+sleep(1)
+driver.find_element(By.XPATH,"//span[text()=' 交卷 ']//..").click()
+sleep(1)
+driver.find_element(By.XPATH,"//span[text()='           确定         ']//..").click()
+sleep(1)
+################################账号注销
+driver.find_element(By.XPATH,"//div[@class='avatar-wrapper el-dropdown-selfdefine     ']").click()
+sleep(1)
+driver.find_element(By.XPATH,"//span[text()='退出登录']//..").click()
+##################
 driver.close()
 #关闭浏览器器进程
 driver.quit()
