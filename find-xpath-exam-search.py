a337fb7b5c6ae7873fdec6b2794bc487ee4507d0
jerffrey <jerffrey_167@163.com>
2022-09-29 15:13:23 +0800

    考试系统搜索框脚本

--- find-xpath-exam-search.py
+++ find-xpath-exam-search.py
@@ -2,7 +2,7 @@ from  selenium import  webdriver
 from  time import  sleep
 from  selenium.webdriver.common.by import By
 '''
-考试系统的登录+输入框的条件查询+账号退出
+考试系统的登录+输入框的条件查询+下拉框的条件查询+账号退出
 '''
 driver = webdriver.Chrome()
 # 打开浏览器
@@ -22,12 +22,14 @@ driver.find_element(By.XPATH,"//*[starts-with(@style,'padding-left: 20px')]/span
 #点击考试管理模块
 sleep(1)
 driver.find_element(By.XPATH,"//*[starts-with(@style,'padding-left: 40px')]/span[text()='考试管理']").click()
-
-sleep(1)
-# driver.find_element(By.XPATH,'//input[@placeholder="开放类型"]').click()
-# driver.find_element(By.XPATH,"//span[text()='完全开放']//..").click()
 sleep(1)
 ######################考试类型搜索框
+driver.find_element(By.XPATH,'//input[@placeholder="开放类型"]').click()
+sleep(1)
+ExamSearch=driver.find_element(By.XPATH,"//div[@class='el-select-dropdown el-popper'and@x-placement]/div")
+ExamSearch1=driver.find_element(By.XPATH,"//span[text()='完全开放']//..").click()
+sleep(1)
+######################考试名称搜索框
 driver.find_element(By.XPATH,'//input[@placeholder="搜索考试名称"]').send_keys('演示考试')
 sleep(1)
 ################################账号注销
