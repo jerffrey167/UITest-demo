ebddb8f7edb98243d2fb6aab962e108007d7b730
jerffrey <jerffrey_167@163.com>
2022-09-29 14:43:27 +0800

    百度登录脚本完成

--- find-xpath-baidu-login.py
+++ find-xpath-baidu-login.py
@@ -8,12 +8,23 @@ driver = webdriver.Chrome()
 # 打开浏览器
 driver.maximize_window()
 #浏览器窗口最大化
-driver.get('http://www.baiud.com')
+driver.get('http://www.baidu.com')
 #浏览器打开百度
 sleep(2)
 driver.find_element(By.XPATH,'//a[@id="s-top-loginbtn"]').click()
-#点击登录按钮
-driver.find_element(By.XPATH,'//input[@type="password"]').send_keys('admin')
+sleep(1)
+#点击弹出登录弹出框
+element=driver.find_element(By.XPATH,"//div[@class='tang-content']")
+element1=element.find_element(By.XPATH,"//input[@id='TANGRAM__PSP_11__userName']")
+element1.clear()
+element1.send_keys('admin')
+element1=element.find_element(By.XPATH,"//input[@id='TANGRAM__PSP_11__password']")
+element1.clear()
+element1.send_keys('123456')
+sleep(1)
+#登录弹出框输入内容
+driver.find_element(By.XPATH,"//input[@id='TANGRAM__PSP_11__submit']").click()
+
 
 ##################
 driver.close()
