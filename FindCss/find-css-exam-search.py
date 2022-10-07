da79d0e58328ad9f79362a78c54ee2522345d2cf
jerffrey <jerffrey_167@163.com>
2022-10-07 15:50:58 +0800

    增加css定位脚本

---
+++ FindCss/find-css-exam-search.py
@@ -0,0 +1,50 @@
+from  selenium import  webdriver
+from  time import  sleep
+from  selenium.webdriver.common.by import By
+'''
+考试系统的登录+输入框的条件查询+下拉框的条件查询+账号退出
+'''
+driver = webdriver.Chrome()
+# 打开浏览器
+driver.maximize_window()
+#浏览器窗口最大化
+driver.get('http://120.46.215.163:8101/#/login')
+#浏览器打开考试系统
+sleep(2)
+#driver.find_element(By.XPATH,'//input[@type="text"]').send_keys('admin')
+driver.find_element(By.CSS_SELECTOR,'input[type="text"]').send_keys('admin')
+#用户名
+#driver.find_element(By.XPATH,'//input[@type="password"]').send_keys('admin')
+driver.find_element(By.CSS_SELECTOR,'input[type="password"]').send_keys('admin')
+#密码
+#driver.find_element(By.XPATH,'//button[@type="button"]').click()
+driver.find_element(By.CSS_SELECTOR,'button[type="button"]').click()
+#登录按钮
+sleep(1)
+driver.find_element(By.XPATH,"//*[starts-with(@style,'padding-left: 20px')]/span[text()='考试管理']/..").click()
+driver.find_element(By.XPATH,"//*[starts-with(@style,'padding-left: 20px')]/span[text()='考试管理']").click()
+#点击考试管理模块
+sleep(1)
+driver.find_element(By.XPATH,"//*[starts-with(@style,'padding-left: 40px')]/span[text()='考试管理']").click()
+sleep(1)
+######################考试类型搜索框
+#driver.find_element(By.XPATH,'//input[@placeholder="开放类型"]').click()
+driver.find_element(By.CSS_SELECTOR,'input[placeholder="开放类型"]').click()
+sleep(1)
+# ExamSearch=driver.find_element(By.XPATH,"//div[@class='el-select-dropdown el-popper'and@x-placement]")
+# ExamSearch1=driver.find_element(By.XPATH,"//span[text()='完全开放']//..").click()
+ExamSearch=driver.find_element(By.CSS_SELECTOR,"div[class='el-select-dropdown el-popper'][x-placement]")
+ExamSearch1=driver.find_element(By.CSS_SELECTOR,"li.el-select-dropdown__item").click()
+sleep(1)
+######################考试名称搜索框
+driver.find_element(By.XPATH,'//input[@placeholder="搜索考试名称"]').send_keys('演示考试')
+sleep(1)
+################################账号注销
+driver.find_element(By.XPATH,"//div[text()=' 超管A ']").click()
+sleep(1)
+driver.find_element(By.XPATH,"//span[text()='退出登录']//..").click()
+##################
+driver.close()
+#关闭浏览器器进程
+driver.quit()
+#关闭浏览器
