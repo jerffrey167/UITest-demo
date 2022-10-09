a160fb71b7c4083d69bfd6a0e05417895462226d
jerffrey <jerffrey_167@163.com>
2022-10-09 09:26:28 +0800

    增加iframe 的切换

---
+++ ElenmentInteractive/IframeSwitch.py
@@ -0,0 +1,21 @@
+from  selenium import  webdriver
+from  time import  sleep
+from  selenium.webdriver.common.by import By
+'''
+考试系统的登录+输入框的条件查询+下拉框的条件查询+账号退出
+'''
+driver = webdriver.Chrome()
+# 打开浏览器
+driver.maximize_window()
+#浏览器窗口最大化
+driver.get('https://mail.163.com/')
+sleep(2)
+iframe = driver.find_element(By.XPATH,'//*[@frameborder="0"]')
+driver.switch_to.frame(iframe)
+
+driver.find_element(By.XPATH,'//*[@name="email"]').send_keys('username')
+
+driver.find_element(By.XPATH,'//*[@name="password"]').send_keys('userpasswd')
+sleep(2)
+driver.find_element(By.XPATH,'//*[@name="email"]').clear()
+driver.find_element(By.XPATH,'//*[@name="password"]').clear()
