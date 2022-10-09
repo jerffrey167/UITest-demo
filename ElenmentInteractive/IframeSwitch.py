05061929a4fea18dd8878ac050397e6e7ffc6e50
jerffrey <jerffrey_167@163.com>
2022-10-09 10:13:32 +0800

    增加iframe 回切

--- ElenmentInteractive/IframeSwitch.py
+++ ElenmentInteractive/IframeSwitch.py
@@ -12,10 +12,17 @@ driver.get('https://mail.163.com/')
 sleep(2)
 iframe = driver.find_element(By.XPATH,'//*[@frameborder="0"]')
 driver.switch_to.frame(iframe)
-
+#############切换到对应的iframe中
 driver.find_element(By.XPATH,'//*[@name="email"]').send_keys('username')
 
 driver.find_element(By.XPATH,'//*[@name="password"]').send_keys('userpasswd')
 sleep(2)
 driver.find_element(By.XPATH,'//*[@name="email"]').clear()
 driver.find_element(By.XPATH,'//*[@name="password"]').clear()
+
+driver.switch_to.default_content()
+###############切换回主干页面
+sleep(1)
+driver.find_element(By.XPATH,"//a[text()='企业邮箱']").click()
+
+
