d15d224969bf031d550bb27abc7997d1cf8eb08d
jerffrey <jerffrey_167@163.com>
2022-11-14 13:37:36 +0800

    删除  close

---
+++ test/python.py
@@ -0,0 +1,10 @@
+import  unittest
+class Testtest(unittest.TestCase):
+    def test_01(self):
+        print("this is test1")
+
+    def test_02(self):
+        print("this is test2")
+
+    def test_03(self):
+        print("this is test3")
