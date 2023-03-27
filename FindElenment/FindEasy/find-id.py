327cd823e8693af04c67beace9dfb0f9bf16d1a8
jerffrey <jerffrey_167@163.com>
2023-03-27 11:27:34 +0800

    修改 id 定位的 输入内容

--- FindElenment/FindEasy/find-id.py
+++ FindElenment/FindEasy/find-id.py
@@ -3,13 +3,14 @@ from  time import  sleep
 from  selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
+
 # 打开浏览器
 driver.maximize_window()
 #浏览器窗口最大化
 driver.get('http://www.baidu.com')
 #浏览器打开百度
 sleep(2)
-driver.find_element(By.ID,'kw').send_keys('id定位')
+driver.find_element(By.ID,'kw').send_keys('selenium')
 #ID定位搜索框输入 selenium
 sleep(1)
 driver.find_element(By.ID,'su').click()
