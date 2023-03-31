eaf264ed5120e9a102099f64c5b0121c73742e16
jerffrey <jerffrey_167@163.com>
2023-03-31 15:06:53 +0800

    维护 图片上传

--- ElenmentInteractive/FileUpload.py
+++ ElenmentInteractive/FileUpload.py
@@ -5,12 +5,12 @@ driver = webdriver.Chrome()
 # 打开浏览器
 driver.maximize_window()
 #浏览器窗口最大化
-driver.get('http://www.baidu.com')
+driver.get('https://image.baidu.com/')
 #浏览器打开考试系统
 driver.implicitly_wait(6)
 #隐式等待
-driver.find_element(By.CSS_SELECTOR,'span.soutu-btn').click()
-driver.find_element(By.CSS_SELECTOR,'input[type=file]').send_keys("C:\\Users\\Administrator\\Pictures\\R-C.png")
+driver.find_element(By.CSS_SELECTOR,'#sttb').click()
+driver.find_element(By.CSS_SELECTOR,'input[type=file]').send_keys("C:\\Users\\jerffrey\\Pictures\\R-C.jpg")
 sleep(5)
 
 driver.quit()
